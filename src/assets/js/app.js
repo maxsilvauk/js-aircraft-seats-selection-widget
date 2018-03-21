2f814c4e46605922911a483a756fd78affa01052
Max Silva <maxs@paxport.net>
2018-03-21 15:26:38 +0000

    Tidy up

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,13 +1,11 @@
 /*jshint esversion: 6 */
+
 (function () {
    'use strict';
 }());
 
 { /* Import Classes */ }
 import RetrieveAvailabilityClass from './RetrieveAvailabilityClass.js';
-
-
-//document.getElementById('retrievalForm').addEventListener('submit', new RetrieveSeatsClass());
 
 {/* On window load get the seats data  */}
 window.addEventListener('load', () => new RetrieveAvailabilityClass());
