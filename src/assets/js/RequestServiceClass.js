35141044e86c628540014d7ed9bc3ebacacc41e8
Max Silva <maxs@paxport.net>
2018-03-22 14:02:27 +0000

    Removed more JSHint stuff. Also worked out that async functions don't compile when in a class.

--- src/assets/js/RequestServiceClass.js
+++ src/assets/js/RequestServiceClass.js
@@ -1,30 +1,18 @@
-/*jshint esversion: 6 */
-
 (function () {
    'use strict';
 }());
 
-{
-  /**
-   * Request Service Class
-   *
-   * Handles network requests.
-   **/
-}  
-export default class RequestServiceClass {
-
-	// async function
-	async function getRequest(url) {
-	 	let data = await (await (fetch(url, {credentials:'include'})
-	    	.then(response => {
-	      		return response.json();
-	    	})
-	    	.catch(error => {
-	      		console.log('Error: ', error);
-
-	    	})
-	 	));
-	  	
-	  	return data;
-	}
+// async function
+export async function getRequestMax(url) {
+	console.log('we get here');
+	let data = await (await (fetch(url, {credentials:'include'})
+		.then(response => {
+  		return response.json();
+		})
+		.catch(error => {
+  			console.log('Error: ', error);
+		})
+	));
+  	
+  	return data;
 }
