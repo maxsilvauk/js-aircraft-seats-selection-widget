e261f6338999c1cde2651e4572344ce988ff7e54
Max Silva <maxsilvauk@gmail.com>
2018-04-17 11:49:02 +0100

    Created a .jsx sourcemap. Added more configuration options and new plugins to help decrease file size. Updated app name

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,39 @@
 const path = require('path');
+const webpack = require('webpack');
 const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': '"production"'
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      mangle: true,
+      compress: {
+        warnings: false, // Suppress uglification warnings
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        screw_ie8: true
+      },
+      output: {
+        comments: false,
+      },
+      exclude: [/\.min\.js$/gi] // skip pre-minified libs
+    }),
+    new webpack.IgnorePlugin(/^\.\/locale$/, [/moment$/]),
+    new webpack.NoEmitOnErrorsPlugin(),
     new CompressionPlugin({
-      asset: "[path].gz[query]",
-      algorithm: "gzip",
-      test: /\.js$/,
+      asset: "[path].gzip[query]",
+      algorithm: "gz",
+      test: /\.js$|\.html$/,
       threshold: 10240,
       minRatio: 0.8
-    })
+    }),
+    new webpack.optimize.AggressiveMergingPlugin()
   ],
+  devtool: 'source-map',
   devServer: {
     compress: true,
     open: true
@@ -22,6 +45,9 @@ module.exports = {
     ]
   },
   output: {
+    devtoolLineToLine: true,
+    sourceMapFilename: "app.bundle.js.map",
+    pathinfo: true,
     path: path.resolve(__dirname, 'dist/js'),
     filename: 'app.bundle.js',
     libraryTarget: 'var',
